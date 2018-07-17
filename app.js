5375c148784461cdcf55c8bef0c325cf27719d87
malavancs <mlvnhari@gmail.com>
2018-07-17 23:10:10 +0530

    App Completed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 var product_listRouter = require('./routes/product_list');
 var app = express();
 
-
+const  cors = require('cors');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products',product_listRouter);
--- routes/product_list.js
+++ routes/product_list.js
@@ -20,7 +20,7 @@ connection.connect(function(err){
 
 
 router.get('/getAllProduct',function (request,response,next) {
-    connection.query("SELECT *FROM product_list",function (err, rows, fields) {
+    connection.query("SELECT *FROM product_list ORDER BY sno ASC",function (err, rows, fields) {
         if (err) throw err
 
         response.setHeader('Content-Type', 'application/json');
@@ -46,6 +46,99 @@ router.post('/addProduct',function (request,response,next) {
    });
 
   //  response.send(postdata);
+
+});
+router.get('/searchTerm',function (request,response,next) {
+    var query = request.query.name;
+    var queryString=('SELECT * FROM product_list WHERE name  LIKE \''+query+'%\'');
+   connection.query(queryString,function (err,rows,fields) {
+       if (err) throw err
+
+       response.setHeader('Content-Type', 'application/json');
+       response.send(JSON.stringify(rows[0]));
+   });
+});
+
+router.post('/addItemToCart',function (request,response,next) {
+   var postdata = request.body;
+    var name = postdata.name;
+    var code = postdata.code;
+    var price = Number(postdata.price);
+    var gstpercentage =Number(postdata.gst);
+    var sessionid = 122;
+    var quantity = Number(postdata.quantity);
+    var priceTotal=(price)+(price*(gstpercentage/100));
+
+    var queryString = "INSERT INTO SINGLE_BILLING VALUES(NULL,'"+name+"','"+code+"',"+price+","+gstpercentage+","+quantity+",NULL,'"+sessionid+"',"+priceTotal+");";
+console.log(queryString);
+    connection.query(queryString,function (err,rows,fields) {
+        if (err) throw err
+
+        response.setHeader('Content-Type', 'application/json');
+        response.send("Suceess");
+    });
+
+});
+
+
+router.get('/getTotalPrice',function (request,response,next) {
+   var sessionid = 122;
+   var queryString = 'SELECT sum(total_price) as TotalSum from single_billing where bill_id='+sessionid;
+    connection.query(queryString,function (err,rows,fields) {
+        if (err) throw err
+
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify(rows[0]));
+    });
+
+});
+
+router.post('/makeBill',function (request,response,next) {
+   var postdata = request.body;
+   var totalPrice = Number(postdata.totalprice);
+   var billid = postdata.billid;
+   var details = postdata.details;
+   var queryString = "INSERT INTO SAVED_BILLING VALUES(NULL,NULL,"+totalPrice+",'"+details+"','"+billid+"');";
+   console.log(queryString);
+
+    connection.query(queryString,function (err,rows,fields) {
+        if (err) throw err
+
+        response.setHeader('Content-Type', 'application/json');
+        response.send("Sucess");
+    });
+
+
+});
+router.get('/getAllInCart',function (request,response,next) {
+
+//     var querySessionId;
+
+    var  querySessionId=122;
+
+
+
+    var queryString = 'SELECT *FROM single_billing where bill_id ='+querySessionId;
+
+    console.log(queryString);
+
+    connection.query(queryString,function (err,rows,fields) {
+        if (err) throw err
+
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify(rows));
+    });
+});
+
+
+
+router.get('/billingHistory',function (request,response,next) {
+    connection.query("SELECT *FROM saved_billing ORDER BY sno ASC",function (err, rows, fields) {
+        if (err) throw err
+
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify(rows));
+    })
 
 });
 module.exports = router;
